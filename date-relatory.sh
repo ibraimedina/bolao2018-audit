307ae214c9a3f6c7d7f5e01fec98aa78d3f77626
Ibraim Medina <ibraimedina@gmail.com>
2018-07-03 22:28:43 -0300

    ADD xpath bin to project

--- date-relatory.sh
+++ date-relatory.sh
@@ -8,9 +8,9 @@ sed -i 's/&raquo;/\ /g' daily-summary/${TODAY}.html
 sed -i '460s/.*/\<\/body\>/' daily-summary/${TODAY}.html
 
 # Extract position, name and points per date
-xpath -q -e '//div[@id="ranking"]//table//tr/td[position()=1]//text()' daily-summary/${TODAY}.html > temp-positions
-xpath -q -e '//div[@id="ranking"]//table//tr/td[position()=2]//text()' daily-summary/${TODAY}.html > temp-names
-xpath -q -e '//div[@id="ranking"]//table//tr/td[position()=3]/span/text()' daily-summary/${TODAY}.html > temp-points
+./xpath -q -e '//div[@id="ranking"]//table//tr/td[position()=1]//text()' daily-summary/${TODAY}.html > temp-positions
+./xpath -q -e '//div[@id="ranking"]//table//tr/td[position()=2]//text()' daily-summary/${TODAY}.html > temp-names
+./xpath -q -e '//div[@id="ranking"]//table//tr/td[position()=3]/span/text()' daily-summary/${TODAY}.html > temp-points
 
 # Unify date file
 paste temp-positions temp-names temp-points > temp-final
